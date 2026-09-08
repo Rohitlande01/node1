db2126f4747cd331dd28d0432c73aa5ec189bfaa
Lande Rohit Santosh <rohitlande215@gmail.com>
2026-09-08 16:40:01 +0530

    Change greeting message in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('<h1> Hello world - Vaijapur</h1>')
+  res.send('<h1> Hello Codingwale - Vaijapur</h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
